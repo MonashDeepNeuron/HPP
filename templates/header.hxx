4911e270319171e63d2690a2f542184b97dc8851
oraqlle <41113853+oraqlle@users.noreply.github.com>
2023-01-09 18:24:47 +1100

    Fixed Ass-1 instructions for submission Created Ass-2 Created header template

---
+++ templates/header.hxx
@@ -0,0 +1,10 @@
+/// Author: First-Name Last-Name (example@email.org)
+/// 
+/// Version: v0.1.0
+///
+/// Date: xx-xx-xxxx
+///
+/// Copyright: Copyright (c) 2023
+/// \file file-name.hxx
+///
+/// \godbolt <link>
